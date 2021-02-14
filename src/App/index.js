4ac439e197e5923ca98be0a8784ef52be752ab90
Chris Calhoun <ctcalhoun2@gmail.com>
2021-02-14 11:39:46 -0600

    added navbar

--- src/App/index.js
+++ src/App/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from '../helpers/Routes';
+import MyNavbar from '../components/MyNavbar';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Router>
+          <MyNavbar />
           <Routes />
         </Router>
       </div>
--- src/components/MyNavbar/index.js
+++ src/components/MyNavbar/index.js
@@ -1,35 +1,15 @@
-import React, { Component } from 'react';
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Navbar,
+} from 'reactstrap';
 
-export default class MyNavbar extends Component {
-  logMeOut = (e) => {
-    e.preventDefault();
-    firebase.auth().signOut();
-  }
-
-  render() {
-    const { authed } = this.props;
-    return (
-      <div className="MyNavbar">
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <span className="navbar-brand" href="#">Pinterest</span>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-            </ul>
-
-            <div className="form-inline my-2 my-lg-0">
-              {authed && (
-                <button className="nav-link btn btn-danger" onClick={this.logMeOut}>Logout</button>
-              )}
-            </div>
-          </div>
-        </nav>
-      </div>
-    );
-  }
+export default function MyNavbar() {
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <Link className="navbar-brand" to='/'>100 day of...</Link>
+      </Navbar>
+    </div>
+  );
 }
