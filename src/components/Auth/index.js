ac214e978dbb70cfe4a7d935ea1e497ce4a13593
Chris Calhoun <ctcalhoun2@gmail.com>
2021-02-13 19:01:32 -0600

    set up google auth.

---
+++ src/components/Auth/index.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import {
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
+import AuthData from '../../helpers/data/authData';
+
+export default class Auth extends Component {
+  logMeOut = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  };
+
+  render() {
+    const { authed } = this.props;
+    return (
+      <>
+        {/* if not logged in, show login button */}
+        {!authed ? (
+          <button
+            className='nav-link btn btn-primary'
+            onClick={AuthData.loginClickEvent}>
+            Login
+          </button>
+        ) : (
+          // else show user image and dropdown caret with logout button and name
+          <>
+            <img
+              className='userInfo'
+              src={authed?.photoURL}
+              alt={authed?.displayName}
+            />
+            <UncontrolledDropdown>
+              <DropdownToggle nav caret></DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>{authed?.displayName}</DropdownItem>
+                <DropdownItem>
+                  <div
+                    className='nav-link btn btn-danger'
+                    onClick={(e) => this.logMeOut(e)}>
+                    Logout
+                  </div>
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </>
+        )}
+      </>
+    );
+  }
+}
