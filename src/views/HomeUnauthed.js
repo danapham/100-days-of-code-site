3ec2177632a00bdd2c2e8017438e9c7f2c684d3b
Chris Calhoun <ctcalhoun2@gmail.com>
2021-02-15 21:22:40 -0600

    added unauthed view.

--- src/views/HomeUnauthed.js
+++ src/views/HomeUnauthed.js
@@ -3,7 +3,13 @@ import React from 'react';
 export default function HomeUnauthed() {
   return (
     <div>
-      <h1>Home Unauthed</h1>
+      <h1>100 Days of Code</h1>
+      <h2>What is this site about?</h2>
+      <br></br>
+      <p>It's about learning a new habit: Coding. Every. Day.</p>
+      <br></br>
+      <p>Dana and I are enrolled in Nashville Software School's Full Stack Web Developer Bootcamp. As we inch closer and closer towards graduation, the job hunt jitters grow stronger. The call to succumb to imposter syndrome grows louder. In order to combat this, we decided to build a simple application capturing our personal journeys to 100 days of coding.
+      </p>
     </div>
   );
 }
