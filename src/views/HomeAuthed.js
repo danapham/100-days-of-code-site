0b7c5425e3a89d9b16c837b6e78bd6f8c48b187c
Chris Calhoun <ctcalhoun2@gmail.com>
2021-02-16 18:41:25 -0600

    created HomeAuthed view.

---
+++ src/views/HomeAuthed.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class HomeAuthed extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Home Authed</h1>
+      </div>
+    );
+  }
+}
